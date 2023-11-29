6b6d6e02a28c06b867d4c14722fc39f0d300ca0f
Bas van Driel <contact@basvandriel.nl>
2023-11-29 17:10:22 +0100

    add

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ use clap::{command, ArgMatches, Command};
 
 const APPHELP: &str = "A Python Package manger written in Rust";
 
+// CommandHandler class -> create(), handle()
+// CommandCollecter -> get_commands(), match()
+
 pub fn create_cli() -> Command {
     Command::new(
         env!("CARGO_CRATE_NAME"), // noformat
@@ -9,7 +12,7 @@ pub fn create_cli() -> Command {
     .about(APPHELP)
     .subcommand_required(true)
     .disable_help_subcommand(true)
-    .subcommands([command!("example")])
+    .subcommands([command!("example"), command!("add")])
 }
 
 pub fn handle_first_match(matches: ArgMatches) {
@@ -17,6 +20,7 @@ pub fn handle_first_match(matches: ArgMatches) {
 
     match payload {
         Some(("example", _matches)) => println!("did it work?"),
+        Some(("add", _matches)) => println!("hjadf"),
 
         // no match
         _ => unreachable!(),
