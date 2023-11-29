2a3c8dbdc77fc3e69d5c910d69cffb06b28fc406
Bas van Driel <contact@basvandriel.nl>
2023-11-29 08:33:32 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,16 @@ use clap::{command, ArgMatches, Command};
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
-const APPNAME: &str = env!("CARGO_CRATE_NAME");
 const APPHELP: &str = "A Python Package manger written in Rust";
 
 pub fn create_cli() -> Command {
-    Command::new(APPNAME)
-        .about(APPHELP)
-        .subcommand_required(true)
-        .disable_help_subcommand(true)
-        .subcommands([command!("example")])
+    Command::new(
+        env!("CARGO_CRATE_NAME"), // noformat
+    )
+    .about(APPHELP)
+    .subcommand_required(true)
+    .disable_help_subcommand(true)
+    .subcommands([command!("example")])
 }
 
 pub fn handle_first_match(matches: ArgMatches) {
@@ -19,7 +20,7 @@ pub fn handle_first_match(matches: ArgMatches) {
     match payload {
         Some(("example", _matches)) => println!("did it work?"),
 
-        // If all subcommands are defined above, anything else is unreachable!()
+        // no match
         _ => unreachable!(),
     };
 }
